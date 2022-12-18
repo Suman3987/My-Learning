321639df3530ff9331afdbbbeb22203c522e8d74
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2022-12-18 18:39:02 +0000

    script update

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 // let count = 0;
 
 // function increment() {
-//   count = count + 1;
+//   count += 1;
 //   countEl.innerText = count;
-//   // countEl
+// countEl
 // }
 // function save() {
 //   console.log(count);
 // }
+//////////////???????
 
 // let username = "per";
 // let message = "you have three new notification";
@@ -19,8 +20,18 @@
 // console.log(messageToUser);
 
 // console.log(messageToUser);
+////////////???????
 
 let name = "john";
 let greeting = "hi, my name is";
 let myGreeting = greeting + "," + name + "!";
 console.log(myGreeting);
+
+let welcomeEl = document.getElementById("welcome-el");
+
+let name = "john";
+let greeting = "hi, my name is";
+
+welcomeEl.innerText = greeting + " " + name;
+
+welcomeEl.innerText += "👋 ";
