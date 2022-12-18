119ab5977e18ede608ec523be43c1372a716b4bb
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2022-12-18 22:44:00 +0000

    js update

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// let countEl = document.getElementById("count-el");
-// let count = 0;
+//
+let saveEl = document.getElementById("save-el");
 
-// function increment() {
-//   count += 1;
-//   countEl.innerText = count;
-// countEl
-// }
-// function save() {
-//   console.log(count);
-// }
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+}
+
+function save() {
+  let saveAll = count + "  - ";
+  saveEl.textContent += saveAll;
+  countEl.textContent = 0;
+  count = 0;
+
+  // console.log(count);
+}
 //////////////???????
 
 // let username = "per";
@@ -22,16 +31,16 @@
 // console.log(messageToUser);
 ////////////???????
 
-let name = "john";
-let greeting = "hi, my name is";
-let myGreeting = greeting + "," + name + "!";
-console.log(myGreeting);
+// let name = "john";
+// let greeting = "hi, my name is";
+// let myGreeting = greeting + "," + name + "!";
+// console.log(myGreeting);
 
-let welcomeEl = document.getElementById("welcome-el");
+// let welcomeEl = document.getElementById("welcome-el");
 
-let name = "john";
-let greeting = "hi, my name is";
+// let name = "john";
+// let greeting = "hi, my name is";
 
-welcomeEl.innerText = greeting + " " + name;
+// welcomeEl.innerText = greeting + " " + name;
 
-welcomeEl.innerText += "👋 ";
+// welcomeEl.innerText += '👋  ';
