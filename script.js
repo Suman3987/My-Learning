fd8ea87efd380ee214d4f218a6a4d43457ff3a03
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2022-12-18 16:08:44 +0000

    html update

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let countEl = document.getElementById("count-el");
-let count = 0;
+// let countEl = document.getElementById("count-el");
+// let count = 0;
 
-function increment() {
-  count = count + 1;
-  countEl.innerText = count;
-  // countEl
-}
-function save() {
-  console.log(count);
-}
+// function increment() {
+//   count = count + 1;
+//   countEl.innerText = count;
+//   // countEl
+// }
+// function save() {
+//   console.log(count);
+// }
+
+// let username = "per";
+// let message = "you have three new notification";
+
+// let messageToUser = message + "," + username + "!";
+// console.log(message + ", " + username + "!");
+
+// console.log(messageToUser);
+
+// console.log(messageToUser);
+
+let name = "john";
+let greeting = "hi, my name is";
+let myGreeting = greeting + "," + name + "!";
+console.log(myGreeting);
