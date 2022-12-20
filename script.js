3045f8ac9f268ec1158a7d81e91e376ef42c458d
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2022-12-20 20:25:32 +0000

    js update

--- script.js
+++ script.js
@@ -46,11 +46,79 @@
 // welcomeEl.innerText += '👋  ';
 
 ///////
-let username = "per";
-// console.log(username);
+// let username = "per";
+// // console.log(username);
 
-let message = "You have three new notifications";
-// console.log(username + " , " + message + " ! ");
+// let message = "You have three new notifications";
+// // console.log(username + " , " + message + " ! ");
 
-let messageToUser = message + ", " + username;
-console.log(messageToUser);
+// let messageToUser = message + ", " + username;
+// console.log(messageToUser);
+
+// let name = " john";
+// let greeting = "Hello , my name is";
+// let myGreeting = greeting + " , " + name;
+
+// console.log(greeting + name);
+
+///////recap
+// let firstName = "john";
+// let lastName = "perry";
+// let fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// let name = "Linda";
+// let greeting = "Hi there";
+
+// function greetingLinda() {
+//   console.log(greeting + " " + name + " ! ");
+// }
+// greetingLinda();
+
+//////////////////////---------
+//////////incrementing and decrementing
+// let myPoints = 3;
+
+// function add3Points() {
+//   myPoints += 3;
+// }
+
+// function remove1Point() {
+//   myPoints -= 1;
+// }
+
+// add3Points();
+// add3Points();
+// add3Points();
+// remove1Point();
+// remove1Point();
+// console.log(myPoints);
+
+////////////////------
+// /////////String & Number
+
+// console.log("2" + 2); //"22"
+// console.log(11 + 7); //18
+// console.log(6 + "5"); //'65'
+// console.log("My points:" + 5 + 9); //'My points:59'
+// console.log(2 + 2); //4
+// console.log("11" + "14"); //'1114'
+
+//////////////------
+/////// time 1hrs 25 min
+// function purchase() {
+//   document.getElementById("error").innerHTML =
+//     "Something went wrong, please try again";
+// }
+
+// let errorParagaph = document.getElementById("error");
+// console.log(errorParagaph);
+
+// function purchase() {
+//   console.log(purchase);
+//   errorParagaph.innerHTML = "Something went wrong, please try again !";
+// }
+
+//////////////------
+/////// Calculator Challenge
