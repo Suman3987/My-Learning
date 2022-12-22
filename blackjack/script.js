2b74a8342e58ac03ab295218ddf8490e805321b2
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2022-12-22 10:17:25 +0000

    if else state

--- blackjack/script.js
+++ blackjack/script.js
@@ -1,4 +1,31 @@
-let firstCard = 6;
-let secondCard = 9;
+// let firstCard = 10;
+// let secondCard = 7;
 
-let sum = firstCard + secondCard;
+// let sum = firstCard + secondCard;
+// if (sum < 21) {
+//   console.log("Do you want to draw a new card ?");
+// } else if (sum === 21) {
+//   console.log("WOHOO ...!! Congratulation You won the  BLACKJACK Game !!");
+// } else {
+//   console.log("You loss the game !!");
+// }
+
+///////-----//////////
+/////////IF ELSE STATEMENT
+// let age = 22;
+
+// if (age < 21) {
+//   console.log("You can not enter the club");
+// } else {
+//   console.log("Welcome");
+// }
+
+/////////
+let age = 105;
+if (age < 100) {
+  console.log("Not elegible");
+} else if (age === 100) {
+  console.log("Here is your birthday card from the king !");
+} else {
+  console.log("Not elegible, you have already gotten one");
+}
