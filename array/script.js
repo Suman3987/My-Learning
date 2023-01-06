ddae3b2061dc88bac7640b1b256094db807b5671
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2023-01-06 13:08:42 +0000

    js update

---
+++ array/script.js
@@ -0,0 +1,45 @@
+// Arrays - ordered lists of items
+
+let featuredPosts = [
+  "Check out my Netflix clone",
+  "Here's the code for my project",
+  "I've just relaunched my portfolio",
+];
+
+let experience = [
+  "CEO at Scrimba",
+  "Frontend developer at Xeneta",
+  "People counter for Norstat",
+];
+
+// let newMessage = "same here !";
+// experience.push("same here !");
+// console.log(experience);
+
+// console.log(experience[1]);
+// console.log(experience[2]);
+// console.log(experience[0]);
+// console.log(experience.length);
+
+// Arrays with multiple data types
+
+// let person = ["john", 35, true];
+
+// adding and removing items from arrays
+
+let cards = [7, 4];
+cards.push(6);
+// console.log(cards);
+
+let message = [
+  "Hey, how's it going?",
+  "I'm great , thank you ! How about you?",
+  "All good. Been working on my portfolio lately",
+];
+
+let newMessage = "same here !";
+
+message.push(newMessage);
+console.log(message);
+message.pop(newMessage);
+console.log(message);
