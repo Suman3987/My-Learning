9c6039529fe741dd27af50da17d320aa2de8af61
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2023-01-06 12:01:01 +0000

    file upload

--- blackjack/script.js
+++ blackjack/script.js
@@ -21,11 +21,65 @@
 // }
 
 /////////
-let age = 105;
-if (age < 100) {
-  console.log("Not elegible");
-} else if (age === 100) {
-  console.log("Here is your birthday card from the king !");
-} else {
-  console.log("Not elegible, you have already gotten one");
+// let age = 105;
+// if (age < 100) {
+//   console.log("Not elegible");
+// } else if (age === 100) {
+//   console.log("Here is your birthday card from the king !");
+// } else {
+//   console.log("Not elegible, you have already gotten one");
+// }
+
+///////blackjack///
+let firstCard = 10;
+let secondCard = 6;
+let cards = [firstCard, secondCard]; //array
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = " ";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
+
+function startGame() {
+  renderGame();
 }
+function renderGame() {
+  cardEl.textContent = "Cards: " + cards[0] + " + " + cards[1];
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card ?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack !";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game ! ";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+  ////log it out
+  // console.log(message);
+}
+function newCard() {
+  console.log("Drawing a new card from the deck !");
+  let card = 5;
+  sum += card;
+  renderGame();
+}
+
+///////// CASH OUT
+// console.log(hasBlackJack);
+// console.log(isAlive);
+
+/////////////boolean///
+// console.log(4 === 3); // false
+// console.log(5 > 2); // true
+// console.log(12 > 12); // false
+// console.log(3 < 0); // false
+// console.log(3 >= 3); // true
+// console.log(11 <= 11); // true
+// console.log(3 <= 2); // false
+
+// .....
