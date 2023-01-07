6a80f9ab879d5e9bb330b5a80dccdd7a8c117c47
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2023-01-07 13:27:14 +0000

    js update

--- counting in js/script.js
+++ counting in js/script.js
@@ -33,3 +33,16 @@ let message = [
 for (let i = 0; i < message.length; i += 1) {
   console.log(message[i]);
 }
+
+let cards = [7, 3, 9];
+for (let i = 0; i < cards.length; i++) {
+  console.log(cards[i]);
+}
+
+/////////
+let sentence = ["Hello", "my", "name", "is", "Per"];
+let greetingEl = document.getElementById("greeting-el");
+
+for (let i = 0; i < sentence.length; i++) {
+  greetingEl.textContent += sentence[i] + " ";
+}
