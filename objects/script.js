90e992d1d31c6f854642eff3dbce2dda4cfb29b1
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2023-01-08 14:29:14 +0000

    file update

--- objects/script.js
+++ objects/script.js
@@ -1,28 +1,40 @@
 // Object - store data in depth -composite / complex data type
 // key -value pairs
 
-let course = {
-  title: "learn CSS Grid for free",
-  lessons: 16,
-  creator: "Per Harald Borgen",
-  length: 63,
-  level: 2,
-  isFree: true,
-  tags: ["html", "css"],
-};
+// let course = {
+//   title: "learn CSS Grid for free",
+//   lessons: 16,
+//   creator: "Per Harald Borgen",
+//   length: 63,
+//   level: 2,
+//   isFree: true,
+//   tags: ["html", "css"],
+// };
 
-console.log(course.isFree);
+// console.log(course.isFree);
 
-let airbnb = {
-  buildBy: "Per",
-  location: "north",
-  rooms: 6,
-  ticket: "true",
-  buildIn: 1896,
-  owners: ["william", "Peter", "Per"],
-  security: "eagle",
-};
-console.log(airbnb.location);
-console.log(airbnb.rooms);
-console.log(airbnb.owners[1]);
-console.log(airbnb.ticket);
+// let airbnb = {
+//   buildBy: "Per",
+//   location: "north",
+//   rooms: 6,
+//   ticket: "true",
+//   buildIn: 1896,
+//   owners: ["william", "Peter", "Per"],
+//   security: "eagle",
+// };
+// console.log(airbnb.location);
+// console.log(airbnb.rooms);
+// console.log(airbnb.owners[1]);
+// console.log(airbnb.ticket);
+
+let person = { name: "Per", age: 35, country: "Norway" };
+function logData() {
+  console.log(
+    person.name +
+      " is " +
+      person.age +
+      " years old and lives in " +
+      person.country
+  );
+}
+logData();
