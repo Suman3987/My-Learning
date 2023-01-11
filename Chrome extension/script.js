43259539c234c39c26901e5a43b174b15d05b032
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2023-01-11 19:16:04 +0000

    js update

---
+++ Chrome extension/script.js
@@ -0,0 +1,10 @@
+let inputBtn = document.getElementById("input-btn");
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked from addEventListener");
+});
+//  reminder(4hour48min for detail )
+
+// let inputBtn = document.getElementById("input-btn");
+// inputBtn.addEventListener("click", function () {
+//   console.log("button clicked");
+// });
