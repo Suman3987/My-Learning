84ea60dfdcd50ad2663538d60d2b92e98661cf1c
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2023-01-13 23:31:35 +0000

    file update

--- Chrome extension/script.js
+++ Chrome extension/script.js
@@ -16,10 +16,23 @@
 //   console.log("I want to open the box!");
 // });
 
-////// your turn to refactor///////
+////// your turn to refactor///////4HR 52MIN
 
-let inputBtn = document.getElementById("input-btn");
+// let inputBtn = document.getElementById("input-btn");
 
+// inputBtn.addEventListener("click", function () {
+//   console.log("Button clicked");
+// });
+
+/////// create th myLeads array and inputEL /////////4HR 52MIN
+let myLeads = [];
+const inputEL = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 inputBtn.addEventListener("click", function () {
   console.log("Button clicked");
 });
+
+///////when to use let and const/////
+
+const playerName = "Per";
+let credits = 45;
