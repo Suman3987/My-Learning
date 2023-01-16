dbd85669a60b938d33d448eb8c81dd0b648abe44
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2023-01-16 19:55:08 +0000

    file update

--- Chrome extension/script.js
+++ Chrome extension/script.js
@@ -95,11 +95,13 @@ const fullPrice = basePrice - discount + shippingCost;
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEL = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-
+const ulEl = document.getElementById("ul-el"); //create the unordered list///
+console.log(ulEl);
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEL.value);
   console.log(myLeads);
 });
+
 for (let i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i]);
+  ulEl.textContent += myLeads[i] + " ";
 }
