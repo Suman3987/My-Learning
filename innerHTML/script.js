a39857fb86d07ef73a11b9370eeb40495ca5695e
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2023-01-17 19:09:19 +0000

    file uploaded

--- innerHTML/script.js
+++ innerHTML/script.js
@@ -5,3 +5,5 @@ container.innerHTML = "<button onclick='buy()'>BUY!</button>";
 function buy() {
   container.innerHTML = "<p>Thank you for buying!</p>";
 }
+
+////// Render the <li></li> elements with innerHTML////5HOUR 16MIN
